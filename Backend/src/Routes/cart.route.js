2a25313eedc5ed49c4a17ff72002a14c92af5641
Vijay-912 <vsaini47428@gmail.com>
2026-03-31 23:50:53 +0530

    fix: CONTROLLER

---
+++ Backend/src/Routes/cart.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { isAuthenticated } from "../Middlewares/isAuth.js";
+import {
+  addToCart,
+  getCart,
+  removeFromCart,
+  updateQuantity,
+} from "../Controllers/Cart.Controller.js";
+
+const router = express.Router();
+
+router.get("/", isAuthenticated, getCart);
+
+router.post("/add", isAuthenticated, addToCart);
+
+router.put("/update", isAuthenticated, updateQuantity);
+
+router.delete("/remove", isAuthenticated, removeFromCart);
+
+export default router;
---
+++ Backend/src/Routes/user.route.js
@@ -0,0 +1,32 @@
+import express from "express";
+import {
+  allUser,
+  changePassword,
+  forgotPassword,
+  getUserById,
+  login,
+  logout,
+  register,
+  reVerify,
+  updateUser,
+  Verify,
+  VerifyOTP,
+} from "../Controllers/User.controller.js"; // Importing controller functions for user-related operations
+import { isAdmin, isAuthenticated } from "../Middlewares/isAuth.js";
+import { singleUpload } from "../Middlewares/multer.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/verify", Verify);
+router.post("/reverify", reVerify);
+router.post("/login", login);
+router.post("/logout", isAuthenticated, logout);
+router.post("/forgot-password", forgotPassword);
+router.post("/verify-otp/:email", VerifyOTP);
+router.post("/change-password/:email", changePassword);
+router.get("/all-user", isAuthenticated, isAdmin, allUser);
+router.get("/get-user/:userId", getUserById);
+router.put("/update/:id", isAuthenticated, singleUpload, updateUser);
+
+export default router;
